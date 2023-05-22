ba6dc5a9fdd2250125cf2df3709a0d938ccce118
Muhammad Dila <mochammeddila21@gmail.com>
2023-05-22 20:37:00 +0700

    Login and Register V2

--- app/src/main/java/com/example/loginandregisterv2/LoginActivity.kt
+++ app/src/main/java/com/example/loginandregisterv2/LoginActivity.kt
@@ -4,47 +4,40 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
 import androidx.core.content.ContextCompat
 
-class RegisterActivity : AppCompatActivity() {
-
+class LoginActivity : AppCompatActivity() {
     // Declaration
-    private lateinit var editNameText: EditText
     private lateinit var editEmailText: EditText
     private lateinit var editPasswordText: EditText
-    private lateinit var editRepeatPasswordText: EditText
-    private lateinit var buttonSignUpRegister: Button
-    private lateinit var buttonSignInRegister: Button
+    private lateinit var buttonSignInLogin: Button
+    private lateinit var buttonSignUpLogin: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.activity_login)
 
         // Initialization variable
-        editNameText = findViewById(R.id.nameRegister)
-        editEmailText = findViewById(R.id.emailRegister)
-        editPasswordText = findViewById(R.id.passwordRegister)
-        editRepeatPasswordText = findViewById(R.id.repeatPasswordRegister)
-        buttonSignUpRegister = findViewById((R.id.button_signup_register))
-        buttonSignInRegister = findViewById((R.id.button_signin_register))
+        editEmailText = findViewById(R.id.email_login)
+        editPasswordText = findViewById(R.id.password_login)
+        buttonSignInLogin = findViewById((R.id.button_signin_login))
+        buttonSignUpLogin = findViewById((R.id.button_signup_login))
 
-        // When button Sign Up is clicked
-        buttonSignUpRegister.setOnClickListener {
+        // When button Sign In is clicked
+        buttonSignInLogin.setOnClickListener {
 
         }
 
-        // When button Sign In is clicked
-        buttonSignInRegister.setOnClickListener {
-            val i = Intent(this, LoginActivity::class.java)
+        // When button Sign Up is clicked
+        buttonSignUpLogin.setOnClickListener {
+            val i = Intent(this, RegisterActivity::class.java)
             startActivity(i)
         }
 
         // Setting color status bar and navigator bar
         statusNavBar()
     }
-
     // Setting color status bar and navigator bar
     private fun statusNavBar() {
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
